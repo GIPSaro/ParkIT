52e34122b3093286ecff17fcad92c4ad1b6c8a7f
Giorgia Ipsaro Passione <145654021+GIPSaro@users.noreply.github.com>
2024-10-04 08:17:39 +0200

    Initial commit

---
+++ src/test/java/giorgiaipsaropassione/ParkIT/ParkItApplicationTests.java
@@ -0,0 +1,13 @@
+package giorgiaipsaropassione.ParkIT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
